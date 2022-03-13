6070acb766543c9d4abf8626e1c2c2fdd5db8116
Wang-zhetao <wzt15857662906@163.com>
2022-03-14 03:31:12 +0800

    基础样式

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -11,12 +11,46 @@ export default {
   props:{
     theme:{
       type:String,
-      default:'button'
+      default:'button',
     },
   },
 }
 </script>
 
 <style lang="scss" scoped>
-div{border: 1px solid red;}
+div{
+  $h:32px;
+  $border-color:#d9d9d9;
+  $color:#333;
+  $blue:#40a9ff;
+  $radius:4px;
+  .u-button{
+    box-sizing: border-box;
+    height: $h;
+    padding: 0 12px;
+    cursor: pointer;
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    white-space: nowrap;
+    background: white;
+    color: $color;
+    border: 1px solid $border-color;
+    box-shadow: 0 1px 0 fade-out(black,0.95);
+    & + &{
+      margin-right: 8px;
+    }
+    &:hover,
+    &:focus{
+      color: $blue;
+      border-color: $blue;
+    }
+    &:focus{
+      outline: none;
+    }
+    &::-moz-focus-inner{
+      border: 0;
+    }
+  }
+}
 </style>
