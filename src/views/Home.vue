d3d04696deb49f4f56fcd94dd300d4a40950d80e
Wang-zhetao <wzt15857662906@163.com>
2022-03-14 00:33:39 +0800

    搭建 HTML 结构

---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div class="topnav">
+      <div class="logo"></div>
+      <div class="menu"></div>
+    </div>
+    <div class="banner">
+      <h1>pandaUI</h1>
+      <h2>一个厉害的UI 框架</h2>
+      <p class="actions">
+        <a>Github</a>
+        <a>开始</a>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "Home"
+}
+</script>
+
