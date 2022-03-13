ef1a0e56b20b0e67610467a4207b99f4837fda2a
Wang-zhetao <wzt15857662906@163.com>
2022-03-14 03:25:30 +0800

    添加 theme 属性

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,20 +1,19 @@
 <template>
-  <div :size="size">
-  <button v-bind="rest">
+  <button class="u-button" :class="{[`theme-${theme}`]:theme}">
     <slot/>
   </button>
-  </div>
 </template>
 
 <script lang="ts">
 export default {
   name: "Button",
   inheritAttrs:false,
-  props:{},
-  setup(props,context){
-    const {size,...rest} = context.attrs;
-    return{size,rest}
-  }
+  props:{
+    theme:{
+      type:String,
+      default:'button'
+    },
+  },
 }
 </script>
 
