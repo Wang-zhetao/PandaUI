07c53d4d187db3a9a04bbbe81664e61878ba4b24
Wang-zhetao <wzt15857662906@163.com>
2022-03-14 20:50:09 +0800

    创建路由

---
+++ src/views/Install.vue
@@ -0,0 +1,22 @@
+<template>
+  <article>
+    <h1>安装</h1>
+    <p>打开终端运行以下命令:</p>
+    <pre><code>npm install panda-ui</code></pre>
+    <p>或</p>
+    <pre><code>yarn add panda-ui</code></pre>
+    <p>
+      下一节：<a href="#/doc/get-started">开始使用</a>
+    </p>
+  </article>
+</template>
+
+<script lang="ts">
+export default {
+  name: "Install.vue"
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
