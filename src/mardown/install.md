aa8f140f739e86e0cf985d1b61b556b346baa60d
Wang-zhetao <wzt15857662906@163.com>
2022-03-14 21:24:24 +0800

    支持 markdown 文件

---
+++ src/mardown/install.md
@@ -0,0 +1,15 @@
+# 安装
+
+打开终端运行 下列命令:
+
+```
+npm install panda-ui
+```
+
+或
+
+```
+yarn add panda-ui
+```
+
+下一节: [开始使用](#/doc/get-started)
--- src/views/Install.vue
+++ src/views/Install.vue
@@ -1,22 +1,17 @@
 <template>
-  <article class="markdown-body">
-    <h1>安装</h1>
-    <p>打开终端运行以下命令:</p>
-    <pre><code>npm install panda-ui</code></pre>
-    <p>或</p>
-    <pre><code>yarn add panda-ui</code></pre>
-    <p>
-      下一节：<a href="#/doc/get-started">开始使用</a>
-    </p>
+  <article class="markdown-body" v-html="md">
   </article>
 </template>
 
-<script lang="ts">
+<script>
+import md from '../mardown/install.md'
 export default {
-  name: "Install.vue"
+  name: "Install.vue",
+  data(){
+    return{
+      md
+    }
+  }
 }
 </script>
 
-<style lang="scss" scoped>
-
-</style>
