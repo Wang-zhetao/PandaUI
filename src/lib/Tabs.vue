96be23ff3c398c1a38622d394ed53242bd3079d4
Wang-zhetao <wzt15857662906@163.com>
2022-03-14 17:37:38 +0800

    优化3

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -45,19 +45,13 @@ export default {
         throw  new Error('Tabs 子标签必须是 Tab')
       }
     })
-    const current = computed(() => {
-      console.log('重新 return')
-      return defaults.filter((tag) => {
-        return tag.props.title === props.selected
-      })[0]
-    })
     const titles = defaults.map((tag) => {
       return tag.props.title
     })
     const select = (title: string) => {
       context.emit('update:selected', title)
     }
-    return {defaults, titles, current, select, selectedItem, indicator, container}
+    return {defaults, titles, select, selectedItem, indicator, container}
   }
 }
 </script>
