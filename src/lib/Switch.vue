2c3a05b45ba04903a055286efdf3e8109830f44e
Wang-zhetao <wzt15857662906@163.com>
2022-03-16 21:35:35 +0800

    改了border-radius: math.div

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -27,21 +27,21 @@ export default {
 
 $h: 22px;
 $h2: $h - 4px;
-.u-switch{height: $h;width: $h*2;border: none;background: #bfbfbf;border-radius: calc(100% - #{$h2});position: relative;
- > span{
-    position: absolute;top: 2px;left: 2px;height: $h2;width: $h2;background: white;
-
-   border-radius: calc(100% - #{$h2});transition: all 250ms;
+.u-switch{
+  height: $h; width: $h * 2; border: none; background: #bfbfbf; border-radius: math.div($h2,2); position: relative;
+  > span{
+    position: absolute; top: 2px; left: 2px; height: $h2; width: $h2; background: white; border-radius: math.div($h2,2); transition: all 250ms;
   }
   &.u-checked{background: #1890ff;
-    > span{left: calc(100% - #{$h2} - 2px);} }
-  &:focus{outline: none;
-    &:active{
-      > span{width: $h + 4px;}
-    }
-    &.u-checked:active{
-      > span{width: $h2+4px; margin-left: -4px;}
-    }
+    > span{left:calc(100% - #{$h2}-2px);}
+  }
+  &:focus{outline: none;}
+  &:active{
+    >span{width: $h2+4px;}
+  }
+  &.checked:active{
+    > span{width: $h2 + 4px;margin-left: -4px;}
   }
 }
+
 </style>
